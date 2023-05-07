19e3f906c2505d588e8f10439f7e9f0ea58763db
Yusuf Msabeh <yussef.msabeh@gmail.com>
2023-05-07 20:25:15 +0300

    feat: favourites button working

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 const themeButton = document.querySelector(".dark-mode-button");
 const themeText = document.querySelector(".dark-mode-button span");
 const moonIcon = document.getElementById("moonIcon");
+
+const favoritesButton = document.querySelector(".favorites-button");
+const favoritesContainer = document.querySelector(".favourites");
+const favoritesIcon = document.getElementById("favoritesIcon");
+
 function switchTheme() {
   currentTheme = document.documentElement.getAttribute("data-theme");
   console.log(currentTheme);
@@ -15,4 +20,20 @@ function switchTheme() {
   }
 }
 
+function toggleFavoritesContainer() {
+  let bottomValue = favoritesContainer.style.bottom;
+  console.log(bottomValue);
+  if (bottomValue != "0px") {
+    favoritesContainer.style.bottom = "0";
+    favoritesIcon.style.color = "var(--heart-color)";
+    favoritesIcon.setAttribute("name", "heart");
+  } else {
+    favoritesContainer.style.bottom = "-1000";
+    favoritesIcon.style.color = "var(--text-body)";
+
+    favoritesIcon.setAttribute("name", "heart-outline");
+  }
+}
+
 themeButton.addEventListener("click", switchTheme);
+favoritesButton.addEventListener("click", toggleFavoritesContainer);
