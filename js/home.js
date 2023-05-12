4fc80b02fec1235b99fef5937e72c81088c210fd
Yusuf Msabeh <yussef.msabeh@gmail.com>
2023-05-12 14:15:45 +0300

    fix: add real data

--- js/home.js
+++ js/home.js
@@ -1,162 +1,236 @@
 const topics = [
   {
-    image: "../assets/accessibility.png",
+    image: "../assets/html.png",
+    category: "Web Development Languages",
     title: "HTML",
     author: "Sarah smith",
   },
   {
     image: "../assets/css.webp",
+    category: "Web Development Languages",
     title: "CSS",
     author: "David Lee",
   },
   {
+    image: "../assets/javascript.jpg",
+    category: "Web Development Languages",
+    title: "JavaScript",
+    author: "John Johnson",
+  },
+  {
     image: "../assets/jquery.png",
+    category: "Web Development Languages",
     title: "jQuery",
     author: "John Johnson",
   },
   {
     image: "../assets/angular.png",
+    category: "Frontend Frameworks and Libraries",
     title: "Angular",
     author: "Jessica Davis",
   },
   {
     image: "../assets/react.webp",
+    category: "Frontend Frameworks and Libraries",
     title: "React",
     author: "Olivia Martinez",
   },
   {
+    image: "../assets/vuejs.jpeg",
+    category: "Frontend Frameworks and Libraries",
+    title: "Vue",
+    author: "Ava Jones",
+  },
+  {
+    image: "../assets/nodejs.webp",
+    category: "Backend Frameworks and Libraries",
+    title: "Node.js",
+    author: "Ava Jones",
+  },
+  {
+    image: "../assets/expressjs.webp",
+    category: "Backend Frameworks and Libraries",
+    title: "Express.js",
+    author: "Ava Jones",
+  },
+  {
+    image: "../assets/ruby.jpeg",
+    category: "Backend Frameworks and Libraries",
+    title: "Ruby on Rails",
+    author: "Ava Jones",
+  },
+  {
+    image: "../assets/django.jpeg",
+    category: "Backend Frameworks and Libraries",
+    title: "Django",
+    author: "Ava Jones",
+  },
+  {
     image: "../assets/flask.webp",
+    category: "Backend Frameworks and Libraries",
     title: "Flask",
     author: "Ethan Thompson",
   },
   {
     image: "../assets/sql.png",
+    category: "Database and APIs",
     title: "SQL",
     author: "Madison Davis",
   },
   {
     image: "../assets/nosql.png",
+    category: "Database and APIs",
     title: "NoSQL",
     author: "Isabella Wilson",
   },
   {
     image: "../assets/restapi.jpeg",
+    category: "Database and APIs",
     title: "REST APIs",
     author: "Jacob Garcia",
   },
   {
     image: "../assets/graphql.png",
     title: "GraphQL",
+    category: "Database and APIs",
     author: "Mia Brown",
   },
   {
     image: "../assets/oauth.png",
+    category: "Web Development Concepts Technologies",
     title: "OAuth",
     author: "Evelyn Nguyen",
   },
   {
     image: "../assets/json.png",
+    category: "Web Development Concepts Technologies",
     title: "JSON",
     author: "Lian Hernandez",
   },
   {
     image: "../assets/ajax.gif",
+    category: "Web Development Concepts Technologies",
     title: "AJAX",
     author: "Avery Perez",
   },
   {
     image: "../assets/wai.jpeg",
+    category: "Web Development Concepts Technologies",
     title: "WebSockets",
     author: "Victoria Kim",
   },
   {
     image: "../assets/responsive.gif",
+    category: "Web Development Concepts Technologies",
     title: "Responsive Design",
     author: "Ryan Jones",
   },
   {
     image: "../assets/accessibility.png",
+    category: "Web Development Concepts Technologies",
     title: "Accessibilty",
     author: "Samantha Martin",
   },
   {
     image: "../assets/ux.jpeg",
+    category: "Web Development Concepts Technologies",
     title: "User Experience(UX)",
     author: "Luke Davis",
   },
   {
-    image: "../assets/ux.jpeg",
+    image: "../assets/ui.png",
+    category: "Web Development Concepts Technologies",
     title: "User Interface (UI)",
     author: "Grace Wilson",
   },
   {
     image: "../assets/design-systems.jpeg",
+    category: "Web Development Concepts Technologies",
     title: "Design Systems",
     author: "Noah Martinez",
   },
   {
     image: "../assets/performance.png",
+    category: "Web Development Concepts Technologies",
     title: "Performance Optimization",
     author: "Chloe Taylor",
   },
   {
     image: "../assets/cross-browser.jpeg",
+    category: "Web Development Concepts Technologies",
     title: "Cross-Compatibility",
     author: "David Lee",
   },
   {
+    image: "../assets/seo.jpeg",
+    category: "Web Development Concepts Technologies",
+    title: "Search Engine Optimization (SEO)",
+    author: "Ava Jones",
+  },
+  {
     image: "../assets/web-security.jpeg",
+    category: "Web Development Concepts Technologies",
     title: "Web Security",
     author: "Gabriel Hernandez",
   },
   {
     image: "../assets/testing.jpeg",
+    category: "Web Development Concepts Technologies",
     title: "Testing and Debugging",
     author: "Avery Perez",
   },
   {
     image: "../assets/ci-cd.jpeg",
+    category: "Web Development Concepts Technologies",
     title:
       "Continuous Integration/Continuous Integration Continuous Integration/Continuous Integration ",
     author: "Madison Davis",
   },
   {
     image: "../assets/devops.png",
+    category: "Web Development Concepts Technologies",
     title: "DevOps",
     author: "Isabella Wilson",
   },
   {
     image: "../assets/cloud.jpeg",
+    category: "Web Development Concepts Technologies",
     title: "Cloud Computing",
     author: "Jacob Garcia",
   },
   {
     image: "../assets/docker.png",
+    category: "Web Development Concepts Technologies",
     title: "Docker",
     author: "Mia Brown",
   },
   {
     image: "../assets/microservices.png",
+    category: "Web Development Concepts Technologies",
     title: "MicroServices",
     author: "Evelyn Nguyen",
   },
   {
     image: "../assets/pwa.png",
+    category: "Web Development Concepts Technologies",
     title: "Progressive Web Apps",
     author: "Lian Hernandez",
   },
   {
     image: "../assets/wai.jpeg",
+    category: "Web Development Concepts Technologies",
     title: "Web Accessibility Initiaive",
     author: "Avery Perez",
   },
   {
     image: "../assets/cms.png",
+    category: "Web Development Concepts Technologies",
     title: "Content Mangment Systems",
     author: "Victoria Kim",
   },
   {
     image: "../assets/analytics.png",
+    category: "Web Development Concepts Technologies",
     title: "Web Analytics",
     author: "Ryan Jones",
   },
@@ -182,7 +256,7 @@ function displayTopics(topics) {
     <img src="${topic.image}" alt="topic" srcset="" />
     <section class="topic-info">
       <section class="topic-name">
-        <p class="category">Category Category Category Category Category Category Category Category Category Category</p>
+        <p class="category">${topic.category}</p>
         <p class="topic-title">${topic.title}</p>
       </section>
       <div class="topic-card-footer">
